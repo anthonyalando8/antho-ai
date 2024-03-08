8ab16da877e8eb9499d7eff2a61cc855d5bbee9b
anthonyalando8 <anthonyalando8@gmail.com>
2024-03-08 18:45:37 +0300

    Allow Host

--- MamaPesa/settings.py
+++ MamaPesa/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7_0r_pc^w1p5ho9wfw3ep)u8(^grx2486f7824^_)df3dbz$^_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['antho-soft-feafcde0b653.herokuapp.com']
 
 
 # Application definition
