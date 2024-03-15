e6b0c4e29ccfdba3d567509b566af841da163f34
anthonyalando8 <anthonyalando8@gmail.com>
2024-03-15 13:02:48 +0300

    Chatbot improved

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("index/", views.index, name="index page"),
+    path("index/",views.index, name="index page"),
     path("", views.home, name="Home"),
     path('airtime/', views.airtime, name='airtime'),
     path('mpesa/', include('mpesa_api.urls')),
