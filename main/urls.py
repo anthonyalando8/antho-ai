30b4a37a5ca89c92df607c57055e20803b442728
anthonyalando8 <anthonyalando8@gmail.com>
2024-04-15 13:55:25 +0300

    Enhanced site map

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'main'
 urlpatterns = [
-    path("",views.index, name="homepage"),
+    path("home/",views.index, name="homepage"),
     path('airtime/', views.airtime, name='airtime'),
     #path('mpesa/', include('mpesa_api.urls')),
     path('chat/', include('chatbot.urls')),
