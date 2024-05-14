633691120ed20fc65f8a6131d6eaa71517ee21d4
anthonyalando8 <anthonyalando8@gmail.com>
2024-05-14 17:44:37 +0300

    cv2 removed

--- vision/views.py
+++ vision/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import cv2 as cv
-import numpy as np
+# import cv2 as cv
+# import numpy as np
 import base64
 # import tensorflow as tf
 
@@ -37,14 +37,14 @@ import base64
 def index(request):
 
     if request.method == "POST":
-        image = request.FILES.get("image-uploaded")
-        image_data = image.read()
-        nparr = np.frombuffer(image_data, np.uint8)
-        image_read = cv.imdecode(nparr, cv.IMREAD_COLOR)
-        h, w = image_read.shape[:2]
-        encoded_image = cv.imencode(".jpg", image_read)
-        base_64_encoded = base64.b64encode(encoded_image)
-        return HttpResponse(base_64_encoded)
+        # image = request.FILES.get("image-uploaded")
+        # image_data = image.read()
+        # nparr = np.frombuffer(image_data, np.uint8)
+        # image_read = cv.imdecode(nparr, cv.IMREAD_COLOR)
+        # h, w = image_read.shape[:2]
+        # encoded_image = cv.imencode(".jpg", image_read)
+        # base_64_encoded = base64.b64encode(encoded_image)
+        return HttpResponse("base_64_encoded")
     else:
         context ={
             "user": request.user,
