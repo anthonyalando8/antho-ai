8e3713548db6abf8b87b20577e7c146d061a6140
anthonyalando8 <anthonyalando8@gmail.com>
2024-05-16 19:51:42 +0300

    js

--- static/js/ai-chats.js
+++ static/js/ai-chats.js
@@ -45,7 +45,9 @@ $(document).ready(function(){
                         htmlData += (message_user_name+response_ai_name)
                         chathistory = element.fields.chatHistory
                     });
+                    htmlData = hljs.highlightElement(htmlData);
                     $("#chat").html(htmlData)
+                    
                     $("#get_ai_chats").attr("value", chathistory)
                     console.log($("#get_ai_chats").attr("value"))
 
@@ -66,7 +68,7 @@ $(document).ready(function(){
     
 });
 
-//$(document).ready(function(){
+$(document).ready(function(){
     var converter = new showdown.Converter();
 
     var user_avatar = "https://ik.imagekit.io/anthonyalando/Soft_Connect/user.png?updatedAt=1682239876486"
@@ -74,116 +76,120 @@ $(document).ready(function(){
     var converted_to_html_user_avatar_name = converter.makeHtml(`<div markdown="1" class="d-flex align-items-center flex-row mx-2 my-2"><div markdown="1">![your image](${user_avatar} =32x32 "You")</div><div markdown="1" class="mx-md-3 mx-2">**You**</div></div>`)
     var converted_to_html_softchat_avatar_name = converter.makeHtml(`<div markdown="1" class="d-flex align-items-center flex-row mx-2 my-2"><div markdown="1">![soft connect logo](${softchat_avatar} =32x32 "SoftChatAI")</div><div markdown="1" class="mx-md-3 mx-2"> **SoftChatAI**</div></div>`)
        
-const form = document.getElementById('form');
+    const form = document.getElementById('form');
 
-// Add an event listener to the form submission
-form.addEventListener('submit', async function(event) {
-    // Prevent the default form submission
-    event.preventDefault();
-    var submitButton = $('#form button[type="submit"]');
+    // Add an event listener to the form submission
+    form.addEventListener('submit', async function(event) {
+        // Prevent the default form submission
+        event.preventDefault();
+        var submitButton = $('#form button[type="submit"]');
 
-    submitButton.html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>');
-    submitButton.attr("disabled","disabled");
-    // Create a FormData object and populate it with form data
-    const formData = new FormData(form);
-    $('#form')[0].reset();
-    // Send a POST request using the Fetch API
-fetch(form.getAttribute("action"), {
-    method: 'POST',
-    body: formData
-})
-.then(response => {
-    // Check if the response is a streaming response
-    if (response.body && response.body.pipeTo) {
-        // Create a new ReadableStream to read the response data
-        const stream = new ReadableStream({
-            start(controller) {
-                const reader = response.body.getReader();
-                
-                // Read chunks of data as they arrive
-                function read() {
-                    reader.read().then(({ done, value }) => {
-                        if (done) {
-                            submitButton.html('<i class="fa-solid fa-paper-plane"></i>');
-                            submitButton.removeAttr("disabled")
-                            $('#id_message').attr('placeholder','Enter message');
-                            $('#id_message').focus();
-                            controller.close();
-                            return;
+        submitButton.html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>');
+        submitButton.attr("disabled","disabled");
+        // Create a FormData object and populate it with form data
+        const formData = new FormData(form);
+        $('#form')[0].reset();
+        // Send a POST request using the Fetch API
+        fetch(form.getAttribute("action"), {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => {
+            // Check if the response is a streaming response
+            if (response.body && response.body.pipeTo) {
+                // Create a new ReadableStream to read the response data
+                const stream = new ReadableStream({
+                    start(controller) {
+                        const reader = response.body.getReader();
+                        
+                        // Read chunks of data as they arrive
+                        function read() {
+                            reader.read().then(({ done, value }) => {
+                                if (done) {
+                                    submitButton.html('<i class="fa-solid fa-paper-plane"></i>');
+                                    submitButton.removeAttr("disabled")
+                                    $('#id_message').attr('placeholder','Enter message');
+                                    $('#id_message').focus();
+                                    controller.close();
+                                    return;
+                                }
+                                // Process the received data (e.g., append to a DOM element)
+                                // Convert Uint8Array to string
+                                const text = new TextDecoder().decode(value);  
+                                console.log(text)
+                                try {
+                                    // Parse the string as JSON
+                                    const jsonData = JSON.parse(text);
+                                    if("is_first" in jsonData && jsonData.is_first){
+                                        $("#chat").append(converted_to_html_user_avatar_name+`<div class="m-2">${jsonData.prompt}</div>`)
+                                        $("#chat").append(converted_to_html_softchat_avatar_name)
+                                        var response_html = converter.makeHtml(jsonData.res)
+                                        $("#chat").append(`<div id="response_${jsonData.message_id}" class="m-2">${response_html}</div>`)
+                                    }
+                                    if("is_error" in jsonData && jsonData.is_error){
+                                        $("#chat").append(
+                                            `
+                                            <div class="border m-2 rounded m-2 p-1 alert alert-danger d-flex align-items-center" role="alert">
+                                                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                                                <div>
+                                                    Error occurred! Refresh this chat <a href="/chat/" class="alert-link">Reload</a>
+                                                </div>
+                                                
+                                            </div>
+                                            <script>
+                                                $("#chat-form").addClass("d-none");
+                                            </script>
+                                            `
+                                        )
+                                        submitButton.html('<i class="fa-solid fa-paper-plane"></i>');
+                                        submitButton.removeAttr("disabled")
+                                    }
+                                    if("is_on_progress" in jsonData && jsonData.is_on_progress){
+                                        var response_html = converter.makeHtml(jsonData.res)
+                                        $(`#response_${jsonData.message_id}`).html(response_html)
+                                    }
+                                    
+                                    $('#top').scrollTop($('#top')[0].scrollHeight);
+
+                                    // Process the JSON data (e.g., append to a DOM element)
+                                } catch (error) {
+                                    console.error('Error parsing JSON:', error);
+                                    controller.error(error);
+                                }
+                                // Continue reading
+                                read();
+                            }).catch(error => {
+                                console.error('Error reading response:', error);
+                                controller.error(error);
+                            });
                         }
-                        // Process the received data (e.g., append to a DOM element)
-                        // Convert Uint8Array to string
-                        const text = new TextDecoder().decode(value);  
-                        console.log(text)
-                        try {
-                            // Parse the string as JSON
-                            const jsonData = JSON.parse(text);
-                            if("is_first" in jsonData && jsonData.is_first){
-                                $("#chat").append(converted_to_html_user_avatar_name+`<div class="m-2">${jsonData.prompt}</div>`)
-                                $("#chat").append(converted_to_html_softchat_avatar_name)
-                                var response_html = converter.makeHtml(jsonData.res)
-                                $("#chat").append(`<div id="response_${jsonData.message_id}" class="m-2">${response_html}</div>`)
-                            }
-                            if("is_error" in jsonData && jsonData.is_error){
-                                $("#chat").append(
-                                    `
-                                    <div class="border m-2 rounded m-2 p-1 alert alert-danger d-flex align-items-center" role="alert">
-                                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-                                        <div>
-                                            Error occurred! Refresh this chat <a href="{% url 'chatbot:chat' %}" class="alert-link">Reload</a>
-                                        </div>
-                                        
-                                    </div>
-                                    <script>
-                                        $("#chat-form").addClass("d-none");
-                                    </script>
-                                    `
-                                )
-                                submitButton.html('<i class="fa-solid fa-paper-plane"></i>');
-                                submitButton.removeAttr("disabled")
-                            }
-                            if("is_on_progress" in jsonData && jsonData.is_on_progress){
-                                var response_html = converter.makeHtml(jsonData.res)
-                                $(`#response_${jsonData.message_id}`).html(response_html)
-                            }
-                            
-                            $('#top').scrollTop($('#top')[0].scrollHeight);
-
-                            // Process the JSON data (e.g., append to a DOM element)
-                        } catch (error) {
-                            console.error('Error parsing JSON:', error);
-                            controller.error(error);
-                        }
-                        // Continue reading
+                        
+                        // Start reading the response stream
                         read();
-                    }).catch(error => {
-                        console.error('Error reading response:', error);
-                        controller.error(error);
-                    });
-                }
+                    }
+                });
                 
-                // Start reading the response stream
-                read();
+                // Pipe the response stream to another stream or process it directly
+                // For example, you can use a TransformStream to process the data
+                // stream.pipeTo(someWritableStream);
+            } else {
+                // Handle non-streaming response (e.g., regular JSON response)
+                return response.json().then(data => {
+                    console.log('Received non-streaming data:', data);
+                });
             }
+        })
+        .catch(error => {
+            // submitButton.html('<i class="fa-solid fa-paper-plane"></i>');
+            // submitButton.removeAttr("disabled")
+            // $('#id_message').attr('placeholder','Enter message');
+            // $('#id_message').focus();
+            console.error('Fetch error:', error);
         });
-        
-        // Pipe the response stream to another stream or process it directly
-        // For example, you can use a TransformStream to process the data
-        // stream.pipeTo(someWritableStream);
-    } else {
-        // Handle non-streaming response (e.g., regular JSON response)
-        return response.json().then(data => {
-            console.log('Received non-streaming data:', data);
-        });
-    }
-})
-.catch(error => {
-    console.error('Fetch error:', error);
-});
+
+    });
 
 });
-
-//});
 
 
 $('#id_message').on('input', function() {
