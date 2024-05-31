028a050486c120e7b5aa446a67b82b2fedc61eec
anthonyalando8 <anthonyalando8@gmail.com>
2024-05-31 21:48:53 +0300

    dynamic styling

--- staticfiles/js/ai-chats.js
+++ staticfiles/js/ai-chats.js
@@ -348,7 +348,32 @@ $(document).ready(function() {
         // Check if the Enter key is pressed
         if (event.keyCode === 13 && !event.shiftKey) {
             event.preventDefault();
-            $('#btn-submit').click();
+            if($("#id_message").val().length != 0)
+                $('#btn-submit').click();
         }
+    });
+});
+
+$(document).ready(function() {
+    function get_element_height() {
+        var new_height = $("#chat-form").outerHeight(true);
+        return new_height;
+    }
+
+    function setSectionHeight(new_height){
+        //return $(".top-section").height(new_height);
+        console.log("newhight:" ,new_height)
+        $(".top-section").css({
+            "height": `calc(100% - ${new_height}px)`,
+            "max-height": `calc(100% - ${new_height}px)`,
+            "min-height": `calc(100% - ${new_height}px)`
+        })
+    }
+    // Call the function once when the document is ready
+    setSectionHeight(get_element_height())
+
+    // Attach the function to the window resize event
+    $(window).resize(function(){
+        setSectionHeight(get_element_height())
     });
 });
