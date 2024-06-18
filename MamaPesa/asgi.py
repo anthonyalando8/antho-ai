16c0214a20f58aa01d9cb6644cdc31fdc2c7197e
anthonyalando8 <anthonyalando8@gmail.com>
2024-06-18 09:40:04 -0400

    proc

--- MamaPesa/asgi.py
+++ MamaPesa/asgi.py
@@ -8,13 +8,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MamaPesa.settings')
 import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from chatbot.routing import websocket_urlpatterns
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MamaPesa.settings')
+
 django.setup()
 #application = get_asgi_application()
 
